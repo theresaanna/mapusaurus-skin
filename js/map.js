18031b76ebc12c3a05ace3e14559798482df086c
Theresa Summa <theresa.summa@cfpb.gov>
2014-05-19 19:42:23 -0300

    creating map obj

--- js/map.js
+++ js/map.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
+    var drawMap = function(censusjson) {
+        var census = JSON.parse(censusjson),
+            lat = parseFloat(census.features[0].properties.intptlat),
+            lon = parseFloat(census.features[0].properties.intptlon),
+            map;
+        console.log(census);
+
+        map = L.map('map').setView([lat, lon], 13);
+    };
+
     $.ajax({
         url: 'maps-results/formatted_smaller_tracts.geojson',
-        dataType: 'jsonp'     
+        success: drawMap
     });
 });
